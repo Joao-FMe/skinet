1a21d531af9aefb9ed38cf05cdba5ac7a0566758
Joao-FMe <63168182+Joao-FMe@users.noreply.github.com>
2021-09-03 10:27:32 +0100

    Initial Commit

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Mvc;
+using API.Data;
+using System.Threading.Tasks;
+using System.Linq;
+using API.Entities;
+using System;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly StoreContext _context;
+        public ProductsController(StoreContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            var products = await _context.Products.ToListAsync();
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> GetProduct(int id)
+        {
+            return await _context.Products.FindAsync(id);
+        }
+    }
+}
